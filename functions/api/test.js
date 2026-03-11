16f3929dd40c4d9c84982ae9d34047439c7067f5
goenktea <83861728+goenktea@users.noreply.github.com>
2026-03-12 03:36:20 +0700

    Add onRequest function to fetch notes from DB

---
+++ functions/api/test.js
@@ -0,0 +1,12 @@
+export async function onRequest(context) {
+
+  const { DB } = context.env
+
+  const result = await DB.prepare(
+    "SELECT * FROM notes"
+  ).all()
+
+  return new Response(JSON.stringify(result.results), {
+    headers: { "content-type": "application/json" }
+  })
+}
