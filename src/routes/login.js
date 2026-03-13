789b993b5f0fb1e0eeab12d341537b23f3eacc30
goenktea <83861728+goenktea@users.noreply.github.com>
2026-03-14 04:05:28 +0700

    Fix formatting and syntax in loginRoute function

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,14 +2,14 @@ import {loginPage} from "../pages/loginPage.js"
 
 export async function loginRoute(request){
 
-if(request.method==="POST"){
+if(request.method === "POST"){
 
 const form = await request.formData()
 
 const u = form.get("username")
 const p = form.get("password")
 
-if(u==="admin" && p==="123456"){
+if(u === "admin" && p === "123456"){
 
 const url = new URL(request.url)
 
@@ -18,13 +18,13 @@ return Response.redirect(url.origin + "/dashboard")
 }
 
 return new Response(loginPage("Login gagal"),{
-headers:{"content-type":"text/html"}
+headers:{ "content-type":"text/html" }
 })
 
 }
 
 return new Response(loginPage(),{
-headers:{"content-type":"text/html"}
+headers:{ "content-type":"text/html" }
 })
 
 }
