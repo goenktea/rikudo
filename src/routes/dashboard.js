add9082e13a70b65300d9d72b0106ec907cd6455
goenktea <83861728+goenktea@users.noreply.github.com>
2026-03-14 06:46:28 +0700

    Update dashboard.js

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,27 +1,25 @@
 import { dashboardPage } from "../pages/dashboardPage"
 
-export async function dashboard(request, env) {
+export async function dashboard(request, env){
 
 const doc = await env.DB
 .prepare("SELECT * FROM documents ORDER BY id DESC LIMIT 1")
 .first()
 
-
-if (!doc) {
-return new Response("Belum ada dokumen. Buat dokumen dulu dari menu ☰")
+if(!doc){
+return new Response("Belum ada dokumen. Buat dokumen baru dari menu ☰")
 }
-
 
 const customers = await env.DB
 .prepare("SELECT * FROM customers WHERE doc_id=? ORDER BY id LIMIT 100")
 .bind(doc.id)
 .all()
 
-
 return new Response(
 dashboardPage(doc, customers.results),
 {
 headers:{ "content-type":"text/html"}
-})
+}
+)
 
 }
