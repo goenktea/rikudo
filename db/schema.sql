960bff70507f28ffebc69015dc7b95af37d56d78
goenktea <83861728+goenktea@users.noreply.github.com>
2026-03-14 03:14:17 +0700

    Add files via upload

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,16 @@
+CREATE TABLE documents (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ name TEXT,
+ created_at TEXT
+);
 
+CREATE TABLE customers (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ document_id INTEGER,
+ name TEXT,
+ address TEXT,
+ phone TEXT,
+ total_bill INTEGER,
+ paid INTEGER,
+ remaining INTEGER
+);
