232a5259047f930df2b5f5b1d2ee022402a763e4
goenktea <83861728+goenktea@users.noreply.github.com>
2026-03-14 06:36:40 +0700

    Update dashboard.js

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -2,23 +2,26 @@ import { dashboardPage } from "../pages/dashboardPage"
 
 export async function dashboard(request, env) {
 
-  const doc = await env.DB
-    .prepare(`SELECT * FROM documents ORDER BY id DESC LIMIT 1`)
-    .first()
+const doc = await env.DB
+.prepare("SELECT * FROM documents ORDER BY id DESC LIMIT 1")
+.first()
 
-  if (!doc) {
-    return Response.redirect("/doc/history", 302)
-  }
 
-  const customers = await env.DB
-    .prepare(`SELECT * FROM customers WHERE doc_id=? ORDER BY id LIMIT 100`)
-    .bind(doc.id)
-    .all()
+if (!doc) {
+return new Response("Belum ada dokumen. Buat dokumen dulu dari menu ☰")
+}
 
-  return new Response(
-    dashboardPage(doc, customers.results),
-    {
-      headers: { "content-type": "text/html" }
-    }
-  )
+
+const customers = await env.DB
+.prepare("SELECT * FROM customers WHERE doc_id=? ORDER BY id LIMIT 100")
+.bind(doc.id)
+.all()
+
+
+return new Response(
+dashboardPage(doc, customers.results),
+{
+headers:{ "content-type":"text/html"}
+})
+
 }
