179e92786fee818fa6bd31ce18786e0e8ac89dcf
goenktea <83861728+goenktea@users.noreply.github.com>
2026-03-14 05:53:03 +0700

    Update dashboard.js

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,14 +1,10 @@
 import {dashboardPage} from "../pages/dashboardPage.js"
 
-/* DASHBOARD */
-
 export async function dashboardRoute(request,env){
 
 const docs = await env.DB.prepare(
 "SELECT * FROM documents ORDER BY id DESC"
 ).all()
-
-/* jika ada dokumen langsung buka terbaru */
 
 if(docs.results.length > 0){
 
@@ -23,8 +19,6 @@ url.origin + "/doc/" + latest,
 
 }
 
-/* jika belum ada dokumen tampilkan halaman history */
-
 return new Response(
 dashboardPage(docs.results),
 {
@@ -32,52 +26,6 @@ headers:{
 "content-type":"text/html;charset=UTF-8"
 }
 }
-)
-
-}
-
-/* BUAT DOKUMEN BARU */
-
-export async function createDoc(request,env){
-
-const form = await request.formData()
-
-const name = form.get("name")
-
-await env.DB.prepare(
-"INSERT INTO documents(name,created_at) VALUES(?,datetime('now'))"
-).bind(name).run()
-
-const url = new URL(request.url)
-
-const latest = await env.DB.prepare(
-"SELECT id FROM documents ORDER BY id DESC LIMIT 1"
-).first()
-
-return Response.redirect(
-url.origin + "/doc/" + latest.id,
-302
-)
-
-}
-
-/* HAPUS DOKUMEN */
-
-export async function deleteDoc(id,request,env){
-
-await env.DB.prepare(
-"DELETE FROM customers WHERE document_id=?"
-).bind(id).run()
-
-await env.DB.prepare(
-"DELETE FROM documents WHERE id=?"
-).bind(id).run()
-
-const url = new URL(request.url)
-
-return Response.redirect(
-url.origin + "/dashboard",
-302
 )
 
 }
